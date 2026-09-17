75e4f302cb2af5f4f87693cbe8d83e3fc3d99b15
zaland-n <2217513@talnet.nl>
2026-09-17 13:26:21 +0200

    Database migratie en Game model

---
+++ database/migrations/2026_09_17_112042_create_games_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::create('games', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('game_name');
+        $table->string('platform');
+        $table->string('genre');
+        $table->decimal('rating', 3, 1);
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
